6d46c28779e83d373b78762641f6d8616a380471
Ingo Fischer <github@fischer-ka.de>
2018-01-14 22:12:19 +0100

    add strict mode and such

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* jshint -W097 */
+// jshint strict:true
+/*jslint node: true */
+/*jslint esversion: 6 */
+'use strict';
+
 var DaikinAC = require('./lib/DaikinAC');
 var DaikinDiscover = require('./lib/DaikinDiscovery');
 
