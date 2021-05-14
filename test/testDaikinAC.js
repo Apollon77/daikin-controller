718dc08cdb667209a1ac3e128cd7ab092080bebb
Yannik Hampe <yzeugs@cipher-code.de>
2021-05-14 16:18:57 +0200

    improve response parser: it failed when a property contained a comma

--- test/testDaikinAC.js
+++ test/testDaikinAC.js
@@ -16,7 +16,7 @@ describe('Test DaikinAC', function() {
     it('constructor without update', function (done) {
         var req =   nock('http://127.0.0.1')
                     .get('/common/basic_info')
-                    .reply(200, 'ret=OK,type=aircon,reg=eu,dst=1,ver=2_6_0,pow=0,err=0,location=0,name=%4b%6c%69%6d%61%20%4a%61%6e%61,icon=0,method=home only,port=30050,id=,pw=,lpw_flag=0,adp_kind=2,pv=0,cpv=0,cpv_minor=00,led=0,en_setzone=1,mac=A408EACC91D4,adp_mode=run,en_hol=0,grp_name=%4b%69%6e%64%65%72,en_grp=1')
+                    .reply(200, 'ret=OK,type=aircon,reg=eu,dst=1,ver=2_6_0,pow=0,err=0,location=0,name=%4b%6c%69%6d%61%20%4a%61%6e%61,icon=0,method=home only,port=30050,id=,pw=,lpw_flag=0,adp_kind=2,pv=0,cpv=0,cpv_minor=00,led=0,en_setzone=1,mac=A408EACC91D4,adp_mode=run,en_hol=0,grp_name=%4b%69%6e%64%65%72,en_grp=1,ssid1=(/) (°,,°) (/)')
                     .get('/aircon/get_model_info')
                     .reply(200, 'ret=OK,model=NOTSUPPORT,type=N,pv=0,cpv=0,mid=NA,s_fdir=1,en_scdltmr=1');
         var daikin = new DaikinAC('127.0.0.1', options, function(err, res) {
