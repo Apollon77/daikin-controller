79afa3c274522ea918b2b11506d5162770f76d4f
Matthias Hanisch <12542802+Matze2@users.noreply.github.com>
2024-09-28 12:09:43 +0200

    Parse also "type" field from get_demand_control response

--- src/models/DemandControl.ts
+++ src/models/DemandControl.ts
@@ -2,6 +2,7 @@ import { DaikinDataParser, ResponseDict } from '../DaikinDataParser';
 import { DaikinResponseCb } from '../DaikinACRequest';
 
 export class DemandControl {
+    public type?: number;
     public enabled?: boolean;
     public mode?: number;
     public maxPower?: number;
@@ -9,6 +10,7 @@ export class DemandControl {
     // ret=OK,type=1,en_demand=1,mode=0,max_pow=55,scdl_per_day=4,moc=0,tuc=0,wec=0,thc=0,frc=0,sac=0,suc=0
     public static parseResponse(dict: ResponseDict, cb: DaikinResponseCb<DemandControl>): void {
         const result = new DemandControl();
+        result.type = DaikinDataParser.resolveInteger(dict, 'type');
         result.enabled = DaikinDataParser.resolveBool(dict, 'en_demand');
         result.mode = DaikinDataParser.resolveInteger(dict, 'mode');
         result.maxPower = DaikinDataParser.resolveInteger(dict, 'max_pow');
